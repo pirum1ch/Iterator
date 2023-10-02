cdaea18ef5b4aaf5c39f4d5f2f042e142ea41bb4
dmitriy.pirumov <dmitry.pirumov@gmail.com>
2023-10-02 22:35:58 +0300

    2nd commit

--- src/main/java/ru/netology_homework/Main.java
+++ src/main/java/ru/netology_homework/Main.java
@@ -2,6 +2,12 @@ package ru.netology_homework;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int r : new Randoms(90, 101)) {
+            System.out.println("Случайное число: " + r);
+            if (r == 100) {
+                System.out.println("Выпало число 100, давайте на этом закончим");
+                break;
+            }
+        }
     }
 }
--- src/main/java/ru/netology_homework/Randoms.java
+++ src/main/java/ru/netology_homework/Randoms.java
@@ -1,2 +1,31 @@
-package ru.netology_homework;public class Randoms {
+package ru.netology_homework;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    private int min;
+    private int max;
+
+    public Randoms(int min, int max) {
+        random = new Random();
+        this.max = max;
+        this.min = min;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt(min, max);
+            }
+        };
+    }
 }
